69881cf942d8e8daaf1a1f755ffd285a3cf270a2
Anton Robitnitskiy <anton.robitnitskiy@namecheap.com>
2021-11-09 01:22:58 +0200

    add cart and table

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,57 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+import './index.scss';
+import logo from './logo.svg';
+
+interface CardProps {
+    img?: string;
+    value: number;
+    //onClick(): any;
+}
+
+const Card: React.FC<CardProps> = ({img, value}: CardProps) => {
+    return (
+        <button
+            className="card"
+            // onClick={onClick()}
+        >
+            <div className="card__inner">
+                <div className="card__back">
+                    <img src={logo} alt="back of a card"/>
+                </div>
+                <div className="card__face">
+                    {value}
+                </div>
+            </div>
+
+        </button>
+    )
+}
+
+class Table extends React.Component {
+    renderCard() {
+        return (
+            <Card value={3}/>
+        );
+    }
+    render() {
+        return (
+            <div className="table">
+                {this.renderCard()}
+            </div>
+        );
+    }
+}
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+        <div className="container">
+            <Table />
+        </div>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
